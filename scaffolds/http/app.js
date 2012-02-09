bfa3c2572fa61f8b8a5b6f7c695a7b5dad1f1c4c
indexzero <charlie.robbins@gmail.com>
2012-02-08 19:06:11 -0500

    [minor] Update scaffolds to setup app config

--- scaffolds/http/app.js
+++ scaffolds/http/app.js
@@ -1,7 +1,11 @@
 var flatiron = require('flatiron'),
+    path = require('path'),
     app = flatiron.app;
 
+app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
+
 app.use(flatiron.plugins.http);
+
 app.router.get('/', function () {
   this.res.json({ 'hello': 'world' })
 });
