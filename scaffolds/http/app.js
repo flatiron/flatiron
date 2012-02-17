3f70ff76ac5ebe138221fc740ba6a9978830c578
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2012-02-17 17:50:15 +0530

    [api] Finished helper plugin

--- scaffolds/http/app.js
+++ scaffolds/http/app.js
@@ -6,7 +6,8 @@ app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
 
 app.use(flatiron.plugins.resourceful, { root: __dirname });
 app.use(flatiron.plugins.plates, { root: __dirname });
+app.use(flatiron.plugins.helper, { root: __dirname });
 app.use(flatiron.plugins.dispatch, { root: __dirname });
-app.use(flatiron.plugins.http, { routes: app.routes });
+app.use(flatiron.plugins.http, { routes: app.routes, copy: app.helpers });
 
 app.start(3000);
---
+++ scaffolds/http/generators/helper.js
@@ -0,0 +1,4 @@
+var app = require('flatiron').app;
+
+app.helpers.#NAME = function () {
+};
