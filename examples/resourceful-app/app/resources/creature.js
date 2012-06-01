05ee0160bd053b955573c6fed24531734da193b2
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2012-06-02 01:43:44 +0530

    [minor] Accessing resources now more user friendly

--- examples/resourceful-app/app/resources/creature.js
+++ examples/resourceful-app/app/resources/creature.js
@@ -1,6 +1,6 @@
-var resourceful = require('resourceful');
+var app = require('../../../../lib/flatiron').app;
 
-var Creature = module.exports = resourceful.define('creature', function () {
+app.r.define('creature', function () {
   this.string('diet');
   this.bool('vertebrate');
   this.array('belly');
