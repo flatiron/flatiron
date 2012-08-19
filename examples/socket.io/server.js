f2bf34807b43cd3a0eabeec7b971629b1aa1ac05
Marak Squires <marak.squires@gmail.com>
2012-08-19 00:42:22 -0700

    [plugins] First pass at socket.io plugin. Working locally.

--- examples/socket.io/server.js
+++ examples/socket.io/server.js
@@ -6,39 +6,31 @@ var flatiron = require('../../lib/flatiron'),
     app      = flatiron.app;
 
 app.use(flatiron.plugins.http, {
-
   before: [function (req, res) {
-
     fs.readFile(__dirname + '/index.html', function (err, data) {
-
       if (err) {
-      	res.writeHead(500);
-      	return res.end('Error loading index.html');
+        res.writeHead(500);
+        return res.end('Error loading index.html');
       }
-
       res.writeHead(200);
       res.end(data);
-
     });
-
   }]
 });
 
+app.use(flatiron.plugins.socketio)
 
 // Set the server to listen on port `8080`.
 // It is important to do this first, as app.server
 // isn't actually created until you start()
-app.start(8080);
-
-
-// Socket.io
-// -------------------------------------------------- //
-
-var io = require('socket.io').listen(app.server);
-
-io.sockets.on('connection', function(socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function(data) {
-    console.log(data);
+app.start(8080, function(){
+  // Socket.io
+  // -------------------------------------------------- //
+  app.io.sockets.on('connection', function(socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function(data) {
+      console.log(data);
+    });
   });
 });
+
